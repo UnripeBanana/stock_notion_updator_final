34d6f12371d11aa4fc3cad1861228a1749d4e24a
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:37:54 +0900

    Update main.py

--- domestic_stock/main.py
+++ domestic_stock/main.py
@@ -2,7 +2,7 @@ from domestic_stock.read import get_ticker
 from domestic_stock.update import update_stock_DB
 from data.domestic_stock import get_naver_prop, get_yfinance_prop
 
-def domestic_stock_main(pages)
+def main_function(pages)
   for page in pages:
   
     ticker = get_ticker(page)
