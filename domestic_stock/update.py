0238a79a85bb3ee2918b841a4ff12acffa766856
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:32:12 +0900

    Update update.py

--- domestic_stock/update.py
+++ domestic_stock/update.py
@@ -4,13 +4,7 @@ from config import NOTION_PRICE_DB_ID
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
 from data.domestic_stock import get_naver_prop, get_yfinance_prop
 
-def update_stock_DB(ticker):
-
-    stock_info = {
-        **get_naver_prop(ticker),
-        **get_yfinance_prop(ticker)
-    }
-              
+def update_stock_DB(stock_info):
     notion.pages.update(
         page_id=page["NOTION_PRICE_DB_ID"],
         properties=stock_info
