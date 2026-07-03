360f89c1fc07d9b76a22c853ac4765b1efa8ef02
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 14:52:23 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ NOTION_TOKEN = os.environ["NOTION_TOKEN"]
 # 국장종목 DB 링크
 NOTION_PRICE_DB_ID = os.environ["NOTION_PRICE_DB_ID"]
 
+# 국장종목 거래내역 DB 링크
+NOTION_TRADE_DB_ID = os.environ["NOTION_TRADE_DB_ID"]
 
 TIMEZONE
