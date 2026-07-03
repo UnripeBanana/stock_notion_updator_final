3278583589291b6bdbc5553628e8dd7e78de7d8a
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 18:44:12 +0900

    Update update.py

--- notion/update.py
+++ notion/update.py
@@ -2,7 +2,7 @@
 
 from config import NOTION_DB
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
-from data.stock import adawd
+from data.stock import give_properties
 import os
 
 NOTION_TOKEN = os.environ["NOTION_TOKEN"]
@@ -41,7 +41,8 @@ def update_trade_page(page_id, remaining=None, profit=None):
     )
 
 
-def 
+def update_price:
+    properties = give_properties()
     notion.pages.update(
         page_id=page["id"],
         properties=properties
