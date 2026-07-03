eb9dcccef2517b9937a123fd505a31ee47d797d2
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 18:39:47 +0900

    Update stock.py

--- data/stock.py
+++ data/stock.py
@@ -39,72 +39,72 @@ def get_naver_price(code):
 
 def update_stock_prices():
 
-        ticker = ticker()
+    ticker = ticker()
 
-        try:
-            # 국내 주식 여부 (네이버 증권)
-            price_info = get_naver_price(ticker)
+    try:
+        # 국내 주식 여부 (네이버 증권)
+        price_info = get_naver_price(ticker)
 
-            current_price = price_info["price"]
-            change = price_info["change"]
-            upanddown = price_info["cr"]
+        current_price = price_info["price"]
+        change = price_info["change"]
+        upanddown = price_info["cr"]
             
-            # 하락이면 음수로 변경
-            if price_info["rf"] == "5":
-                change = -change
-                upanddown = -upanddown
+        # 하락이면 음수로 변경
+        if price_info["rf"] == "5":
+            change = -change
+            upanddown = -upanddown
 
-            countOfListedStock = price_info["countOfListedStock"]
+        countOfListedStock = price_info["countOfListedStock"]
 
-            ###################################
+        ###################################
             
-            # y_finance
-            stock = yf.Ticker(f"{ticker}.KS")
+        # y_finance
+        stock = yf.Ticker(f"{ticker}.KS")
             
-            info = stock.info
-            market_cap = info.get("marketCap", 0)
+        info = stock.info
+        market_cap = info.get("marketCap", 0)
             
-            if market_cap is None:
-                market_cap = 0
+        if market_cap is None:
+            market_cap = 0
 
-            high_52 = info.get("fiftyTwoWeekHigh")
-            low_52 = info.get("fiftyTwoWeekLow")
+        high_52 = info.get("fiftyTwoWeekHigh")
+        low_52 = info.get("fiftyTwoWeekLow")
 
-            currency = info.get("currency")
-            country = info.get("country")
-            sector = info.get("sector")
-            industry = info.get("industry")
+        currency = info.get("currency")
+        country = info.get("country")
+        sector = info.get("sector")
+        industry = info.get("industry")
 
-            properties = {
-                "현재가_깃허브_원본": {
-                    "number": current_price
-                },
-                "전일대비_깃허브": {
-                    "number": change
-                },
-                "등락률_깃허브_원본": {
-                    "number": upanddown
-                },
-                "시가총액_깃허브": {
-                    "number": countOfListedStock*current_price
-                },
-                "52주 최고가": {
-                    "number": high_52
-                },
-                "52주 최저가": {
-                    "number": low_52
-                },
-                "통화": rich_text(currency),
-                "국가": rich_text(country),
-                "업종": rich_text(sector),
-                "산업": rich_text(industry),
-                "마지막 업데이트": rich_text(logger())  # 업데이트 시간 기록
-            }
+        properties = {
+            "현재가_깃허브_원본": {
+                "number": current_price
+            },
+            "전일대비_깃허브": {
+                "number": change
+            },
+            "등락률_깃허브_원본": {
+                "number": upanddown
+            },
+            "시가총액_깃허브": {
+                "number": countOfListedStock*current_price
+            },
+            "52주 최고가": {
+                "number": high_52
+            },
+            "52주 최저가": {
+                "number": low_52
+            },
+            "통화": rich_text(currency),
+            "국가": rich_text(country),
+            "업종": rich_text(sector),
+            "산업": rich_text(industry),
+            "마지막 업데이트": rich_text(logger())  # 업데이트 시간 기록
+        }
 
-            notion.pages.update(
-                page_id=page["id"],
-                properties=properties
-            )
+        notion.pages.update(
+            page_id=page["id"],
+            properties=properties
+        )
 
-        except Exception as e:
-            print(f"❌ {ticker} 오류: {e}")
+    except Exception as e:
+        print(f"❌ {ticker} 오류: {e}")
