f4b6e10c7671c3b22b028c66c107cf5ed6ef0754
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:56:42 +0900

    Update domestic_stock.py

--- data/domestic_stock.py
+++ data/domestic_stock.py
@@ -2,10 +2,8 @@
 
 from config import NOTION_TOKEN, NOTION_PRICE_DB_ID, NOTION_TRADE_DB_ID
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
-from utils.logger import logger
 import yfinance as yf      # Yahoo 증권에서 데이터 받아오기
 import requests            # 네이버 증권에서 데이터 받아오기
-from utils import logging
 
 def get_naver_prop(ticker):
 
