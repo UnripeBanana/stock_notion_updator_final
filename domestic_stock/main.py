c5a61bb25043cc8cf9dfa73ba52c20fdf884e87a
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:27:53 +0900

    Update main.py

--- domestic_stock/main.py
+++ domestic_stock/main.py
@@ -3,13 +3,6 @@ from notion.get_all_pages import get_all_pages
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
 
 
-for page in get_all_pages(DATABASE_ID):
+for page in get_all_pages(NOTION_PRICE_DB_ID):
 
-  props = page["properties"]
-
-  ticker_data = props["티커"]["rich_text"]
-
-  if len(ticker_data) == 0:
-      continue
-
-  ticker = ticker_data[0]["plain_text"]
+  ticker = 
