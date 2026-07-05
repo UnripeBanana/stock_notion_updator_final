e3529a1f3ca28c0fb0b93d614bdc8b954988f68f
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-05 15:36:27 +0900

    Update domestic_gold.py

--- data/domestic_gold.py
+++ data/domestic_gold.py
@@ -4,15 +4,24 @@ import requests            # 네이버 증권에서 데이터 받아오기
 
 def get_gold_price():
 
-    url = "https://m.stock.naver.com/front-api/realTime/marketIndex/metals"
-
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
 
+    url = f"https://m.stock.naver.com/front-api/realTime/marketIndex/metals/M04020000"
+
+    gold = requests.get(
+        url,
+        headers=headers,
+        timeout=10 # 최대 10초까지만 기다리겠다는 의미.
+    ).json()
+
+    
     response = requests.get(url, headers=headers)
     response.raise_for_status()
 
+    
+    
     gold = response.json()["result"]["metals"]["M04020000"]
 
     return {
@@ -35,3 +44,30 @@ value = gold["accumulatedTradingValue"]     # 거래대금
 status = gold["marketStatus"]               # OPEN / CLOSE
 updated = gold["localTradedAt"]             # 갱신시각
 """
+def get_naver_prop(ticker):
+
+    # 네이버는 브라우저가 아닌 프로그램의 요청을 차단하는 경우가 있어서, 브라우저인 척 속이는 역할 수행
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    url = (
+        f"https://polling.finance.naver.com/api/realtime"
+        f"?query=SERVICE_ITEM:{ticker}"
+    )
+
+    data = requests.get(
+        url,
+        headers=headers,
+        timeout=10 # 최대 10초까지만 기다리겠다는 의미.
+    ).json()
+
+    item = data["result"]["areas"][0]["datas"][0]
+  
+    return {
+        "price": item["nv"],   # 현재가
+        "change": item["cv"],  # 전일 대비 가격 변화(원)
+        "rf": item["rf"],      # 등락 구분(상승/하락/보합을 나타내는 코드)
+        "cr": item["cr"],      # 등락률(%)
+        "countOfListedStock": item["countOfListedStock"]  # 상장주식수
+    }
