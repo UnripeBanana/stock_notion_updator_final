d9379f4072277814149d11db175e0414ebaeeab4
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-05 15:15:11 +0900

    Update main.py

--- gold/main.py
+++ gold/main.py
@@ -1,6 +1,25 @@
+from domestic_gold.update import 
 from data.domestic_gold import get_gold_price
 
 
+from domestic_stock.read import get_ticker
+from domestic_stock.update import update_stock_DB
+from data.domestic_stock import get_naver_prop, get_yfinance_prop
+
+def domestic_stock_main (pages):
+  for page in pages:
+  
+    ticker = get_ticker(page)
+
+    if not ticker:
+      continue
+    
+    stock_info = {
+        **get_naver_prop(ticker),
+        **get_yfinance_prop(ticker)
+    }
+  
+    update_stock_DB(page, stock_info)
 
 """
   "price": int(gold["closePrice"].replace(",", "")),           # 현재가
