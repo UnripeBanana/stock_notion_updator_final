09fb6393de7eb0dd3c188d7c979a7f39cfeb0b38
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-05 16:01:32 +0900

    Update main.py

--- gold/main.py
+++ gold/main.py
@@ -1,10 +1,11 @@
 from gold.update import update_KRX_GOLD_INFO_DB
 from data.domestic_gold import get_gold_price
 
-def gold_main ():
-  gold_info = get_gold_price()
-
-  update_KRX_GOLD_INFO_DB(gold_info)
+def gold_main (pages):
+  for page in pages:
+    gold_info = get_gold_price(page)
+  
+    update_KRX_GOLD_INFO_DB(gold_info)
 
 """
   "price": int(gold["closePrice"].replace(",", "")),           # 현재가
