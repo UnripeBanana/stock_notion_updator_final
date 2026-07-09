58f80cf9e471dbd34c75e7e917f2cf7ae8246e19
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-09 20:30:15 +0900

    Update domestic_bond_etf.py

--- data/domestic_bond_etf.py
+++ data/domestic_bond_etf.py
@@ -1,4 +1,39 @@
-# 채권 ETF
+import requests            # 네이버 증권에서 데이터 받아오기
 
-from config import NOTION_TOKEN, NOTION_PRICE_DB_ID, NOTION_TRADE_DB_ID
-from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
+def get_naver_prop(ticker):
+
+    # 네이버는 브라우저가 아닌 프로그램의 요청을 차단하는 경우가 있어서, 브라우저인 척 속이는 역할 수행
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    url = (
+        f"https://polling.finance.naver.com/api/realtime"
+        f"?query=SERVICE_ITEM:{ticker}"
+    )
+
+    data = requests.get(
+        url,
+        headers=headers,
+        timeout=10 # 최대 10초까지만 기다리겠다는 의미.
+    ).json()
+
+    item = data["result"]["areas"][0]["datas"][0]
+
+    # 임시로 넣은 코드
+    import json
+    print(json.dumps(item, indent=4, ensure_ascii=False))
+  
+    return {
+        "price": item["nv"],   # 현재가
+        "change": item["cv"],  # 전일 대비 가격 변화(원)
+        "rf": item["rf"],      # 등락 구분(상승/하락/보합을 나타내는 코드)
+        "cr": item["cr"],      # 등락률(%)
+
+        "open": item["ov"],
+        "high": item["hv"],
+        "low": item["lv"],
+        "volume": item["aq"],
+        
+        "countOfListedStock": item["countOfListedStock"]  # 상장주식수
+    }
